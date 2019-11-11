689e6043947ad1399973b80fbed9010d8151fd96
ericbanker12 <ericbanker12@gmail.com>
2019-11-11 15:57:21 -0600

    add POST to login as a user

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const bcrypt = require('bcryptjs')
 
 const db = require('./auth-model')
 
@@ -8,6 +9,30 @@ router.post('/register', validateBody, validateUsername, hashPassword, (req, res
     db.add(res.locals.user)
         .then(resp => {
             res.status(201).json({id: resp[0]})
+        })
+        .catch(err => {
+            console.error(err)
+            res.sendStatus(500)
+        })
+})
+
+router.post('/login', validateBody, (req, res) => {
+    const {username, password} = res.locals.user
+
+    db.find({username})
+        .then(user => {
+            if (user) {
+                bcrypt.compare(password, user.password, (err, match) => {
+                    if (err) {
+                        throw err
+                    }
+                    else {
+                        if (match) res.json({message: `Hello ${user.username}`, id: user.id})
+                        else res.status(401).json({message: 'You shall not pass!'})
+                    }
+                })
+            }
+            else res.status(401).json({message: 'You shall not pass!'})
         })
         .catch(err => {
             console.error(err)
