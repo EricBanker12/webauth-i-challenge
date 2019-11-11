5158f67e215f45f5ae0a35e63c8ea381ce9f5726
ericbanker12 <ericbanker12@gmail.com>
2019-11-11 16:55:55 -0600

    add global middleware to /api/restricted

--- api/server.js
+++ api/server.js
@@ -5,6 +5,9 @@ const cors = require('cors')
 const authRouter = require('./auth/auth-router')
 const usersRouter = require('./users/users-router')
 
+const {validateUsername, validatePassword} = require('./auth/auth-middleware')
+const {validateHeaders} = require('./users/users-middleware')
+
 const server = express()
 
 // global middleware
@@ -15,5 +18,11 @@ server.use(express.json())
 // routers
 server.use('/api', authRouter)
 server.use('/api/users', usersRouter)
+server.use('/api/restricted', validateHeaders, validateUsername, validatePassword)
+
+server.get('/api/restricted/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
 
 module.exports = server
